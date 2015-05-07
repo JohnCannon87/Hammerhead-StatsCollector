1bc9c33656d4b9d73520eee4795f016d3162c9ef
JohnCannon87 <john.work.cannon@gmail.com>
2015-05-07 17:41:10 +0100

    Finished Angular Form Submission, Added Bootstrap Support

--- StatsCollector/src/main/webapp/javascript/gerrit-stats-controller.js
+++ StatsCollector/src/main/webapp/javascript/gerrit-stats-controller.js
@@ -1,10 +1,14 @@
-angular.module('appGerritStats').controller('GerritCtrl', ['$scope', '$log', '$q', 'gerritAppConfig', 'Gerrit',
-    function($scope, $log, $q, gerritAppConfig, Gerrit) {
+function UpdateGerritConfig(data, $scope){
+	$scope.gerritHostName = data.host;
+	$scope.reviewersToIgnore = data.reviewersToIgnore;
+}
+
+function GerritStats($http, $scope, $log, $q, gerritAppConfig, Gerrit) {
 	$scope.metrics = new Array();
 	$scope.goals = gerritAppConfig.gerrit.goals;	
-	
+		
 	Gerrit.hostname().then(function(response){
-		$scope.gerritHostName = response.data.host;
+		UpdateGerritConfig(response.data, $scope);
 	});
 	
 	Gerrit.metrics().then(function(response){
@@ -14,4 +18,38 @@ angular.module('appGerritStats').controller('GerritCtrl', ['$scope', '$log', '$q
 		$scope.collabrativeDevelopment = response.data.collabrativeDevelopmentCount;
 		$scope.totalReviews = response.data.totalReviewsCount;
 	});
-}]);
+	
+	$scope.addReviewer = function(){
+		$http.post('/gerrit/config/addReviewerToIgnore?reviewer='+$scope.gerritReviewer)
+		.success(function(data){
+			console.log(data);			
+			UpdateGerritConfig(data, $scope);
+		}).error(function(data){
+			console.log(data);
+		});
+	}
+	
+	$scope.removeReviewer = function(reviewer){
+		$http.post('/gerrit/config/removeReviewerToIgnore?reviewer='+reviewer)
+		.success(function(data){
+			console.log(data);			
+			UpdateGerritConfig(data, $scope);
+		}).error(function(data){
+			console.log(data);
+		});
+	};
+	
+	//Form Data Setup
+	$scope.changeHostName = function(){
+		$http.post('/gerrit/config/changeHost?host='+$scope.gerritHostName)
+		.success(function(data){
+			console.log(data);
+			UpdateGerritConfig(data, $scope);
+		}).error(function(data){
+			console.log(data);
+		});
+	};
+	
+};
+
+angular.module('appGerritStats').controller('GerritCtrl', ['$http', '$scope', '$log', '$q', 'gerritAppConfig', 'Gerrit', GerritStats]);
