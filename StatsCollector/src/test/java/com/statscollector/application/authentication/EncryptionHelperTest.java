ed3ad869eb4843d9f77e2a26f80ec1dd73644cc7
JohnCannon87 <john.work.cannon@gmail.com>
2015-05-29 15:36:13 +0100

    Removed redundant code.

--- StatsCollector/src/test/java/com/statscollector/application/authentication/EncryptionHelperTest.java
+++ StatsCollector/src/test/java/com/statscollector/application/authentication/EncryptionHelperTest.java
@@ -4,17 +4,11 @@ import static org.junit.Assert.assertEquals;
 
 import java.security.NoSuchAlgorithmException;
 
-import javax.crypto.KeyGenerator;
-import javax.crypto.SecretKey;
-
 import org.junit.Test;
 
 public class EncryptionHelperTest {
 	@Test
 	public void testEncryptAndDecryptPassword() throws NoSuchAlgorithmException {
-		KeyGenerator instance = KeyGenerator.getInstance("AES");
-		SecretKey generateKey = instance.generateKey();
-		generateKey.getEncoded().toString();
 		String testPassword = "testPassword";
 		String encryptedPassword = EncryptionHelper.encryptPassword(testPassword);
 		String decryptedPassword = EncryptionHelper.decryptPassword(encryptedPassword);
